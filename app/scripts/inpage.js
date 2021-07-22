cdebeee274d6e4603fa5485cf45f14988b99dbfb
Lightstreams <@lightstreams.io>
2021-07-22 17:19:12 +0100

    restore web3 injection

--- app/scripts/inpage.js
+++ app/scripts/inpage.js
@@ -1,31 +1,61 @@
-
-
 // need to make sure we aren't affected by overlapping namespaces
 // and that we dont affect the app with our namespace
 // mostly a fix for web3's BigNumber if AMD's "define" is defined...
-var __define
+let __define
 
 /**
  * Caches reference to global define object and deletes it to
  * avoid conflicts with other global define objects, such as
  * AMD's define function
  */
-function cleanContextForImports () {
+const cleanContextForImports = () => {
   __define = global.define
   try {
     global.define = undefined
   } catch (_) {
-    console.warn('MetaMask - global.define could not be deleted.')
+    console.warn('Lightstreams Wallet - global.define could not be deleted.')
   }
 }
 
 /**
  * Restores global define object from cached reference
  */
-function restoreContextAfterImports () {
+const restoreContextAfterImports = () => {
   try {
     global.define = __define
   } catch (_) {
-    console.warn('MetaMask - global.define could not be overwritten.')
+    console.warn('Lightstreams Wallet - global.define could not be overwritten.')
   }
 }
+
+cleanContextForImports()
+
+import log from 'loglevel'
+import LocalMessageDuplexStream from 'post-message-stream'
+import { MetaMaskInpageProvider } from 'nifty-wallet-inpage-provider'
+
+// TODO:deprecate:Q1-2020
+import 'web3/dist/web3.min.js'
+
+restoreContextAfterImports()
+
+log.setDefaultLevel(process.env.METAMASK_DEBUG ? 'debug' : 'warn')
+
+//
+// setup plugin communication
+//
+
+// setup background connection
+const metamaskStream = new LocalMessageDuplexStream({
+  name: 'nifty-inpage',
+  target: 'nifty-contentscript',
+})
+
+// compose the inpage provider
+const inpageProvider = new MetaMaskInpageProvider(metamaskStream)
+
+//
+// end deprecate:Q1-2020
+//
+
+window.ethereum = inpageProvider
